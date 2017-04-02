899e81d5ffa46b14d5866c91d5b7c198582ca023
ji5485 <ji5485@naver.com>
2017-04-02 14:59:51 +0900

    Second Upload (final)
    Firebase applied
    Fixed Bugs
    Project Complete

--- public/calendarCtrl.js
+++ public/calendarCtrl.js
@@ -3,7 +3,6 @@ var fullDate;
 
 window.onload = function() {
   calendarSet();
-  planArea();
 }
 
 var calendarSet = function(date) {
@@ -24,13 +23,28 @@ var calendarSet = function(date) {
   if (currentMonth < 10) var currentFullMonth = "0" + currentMonth;
   else currentFullMonth = currentMonth;
 
+  var currentFullDate = currentYear + "년 " + currentMonth + "월";
+  dbGet(currentFullDate);
+
   // 전달
-  if (currentMonth === 1) var prevDate = [currentYear - 1, 12, currentDay];
-  else var prevDate = [currentYear, currentMonth - 1, currentDay];
+  if (currentMonth === 1) {
+    var prevDate = [currentYear - 1, 12, currentDay];
+    var prevFullDate = (currentYear - 1) + "년 12월";
+  }
+  else {
+    var prevDate = [currentYear, currentMonth - 1, currentDay];
+    var prevFullDate = currentYear + "년 " + (currentMonth - 1) + "월";
+  }
 
   // 다음달
-  if (currentMonth === 12) var nextDate = [currentYear + 1, 1, currentDay];
-  else var nextDate = [currentYear, currentMonth + 1, currentDay];
+  if (currentMonth === 12) {
+    var nextDate = [currentYear + 1, 1, currentDay];
+    var nextFullDate = (currentYear + 1) + "년 1월";
+  }
+  else {
+    var nextDate = [currentYear, currentMonth + 1, currentDay];
+    var nextFullDate = currentYear + "년 " + (currentMonth + 1) + "월";
+  }
 
   fullDate = currentYear + "년 " + currentMonth + "월"
   var today = '<span><a href="#" class="glyphicon glyphicon-menu-left" onClick="calendarSet(\''+prevDate+'\')"></a> </span><span ng-model="days">'
@@ -62,8 +76,6 @@ var calendarSet = function(date) {
   htmlCalendar.innerHTML = calendar;
   calendarHead.innerHTML = today;
   monthYear.innerHTML = fullDate;
-
-  planArea();
 };
 
 var transfer = function(date) {
@@ -71,145 +83,4 @@ var transfer = function(date) {
   date = date.split(",");
   date = date[0] + "년 " + date[1] + "월 " + date[2] + "일";
   inputDate.value = date;
-};
-
-var clearForm = function() {
-  var date = document.getElementById('Date');
-  var period = document.getElementById('period');
-  var name = document.getElementById('name');
-  var subject = document.getElementById('subject');
-  var discription = document.getElementById('discription');
-
-  date.value = "";
-  period.value = "";
-  name.value = "";
-  subject.value = "";
-  discription.value = "";
-};
-
-var submitForm = function() {
-  var date = document.getElementById("Date");
-  var period = document.getElementById("period");
-  var name = document.getElementById("name");
-  var subject = document.getElementById("subject");
-  var discription = document.getElementById("discription");
-  var monthYear = document.getElementById("monthYear");
-
-  if (date.value == "" || name.value == "" || subject.value == "" || discription.value == "") {
-    if (date.value == "") {
-      var dateScript = document.getElementById('dateScript');
-      dateScript.innerHTML = "입력되지 않았습니다.";
-      setTimeout(function() {dateScript.innerHTML = ""}, 2000);
-    }
-    if (name.value == "") {
-      var nameScript = document.getElementById('nameScript');
-      nameScript.innerHTML = "입력되지 않았습니다.";
-      setTimeout(function() {nameScript.innerHTML = ""}, 2000);
-    }
-    if (subject.value == "") {
-      var subjectScript = document.getElementById('subjectScript');
-      subjectScript.innerHTML = "선택되지 않았습니다.";
-      setTimeout(function() {subjectScript.innerHTML = ""}, 2000);
-    }
-    if (discription.value == "") {
-      var discriptionScript = document.getElementById('discriptionScript');
-      discriptionScript.innerHTML = "입력되지 않았습니다.";
-      setTimeout(function() {discriptionScript.innerHTML = ""}, 2000);
-    }
-  }
-  else {
-    plan.push({
-      date: date.value,
-      period: period.value,
-      name: name.value,
-      subject: subject.value,
-      discription: discription.value,
-      identifying: monthYear.innerHTML
-    });
-    clearForm();
-    planArea();
-  }
-};
-
-var planArea = function() {
-  var monthPlan = document.getElementById('monthPlan');
-  var Module = "";
-
-  for (var i=0;i<plan.length;i++) {
-    if (plan[i].identifying == fullDate) {
-      Module += '<div class="planModule">';
-      Module += '<button onclick="deleteModule('+i+')">삭제</button>'
-      Module += '<button onclick="popupOn('+i+')">자세히</button>'
-      Module += '<div>'
-      Module += '<span>'+plan[i].name+'</span> <br />';
-      Module += '<span>'+plan[i].subject+'</span>';
-      Module += '</div>'
-      Module += '</div>';
-    }
-  }
-  monthPlan.innerHTML = Module;
-}
-
-var deleteModule = function(value) {
-  plan.splice(value, 1);
-  planArea();
-}
-
-var popupOn = function(value) {
-  var popupName = document.getElementById('popupName');
-  var popupPeriod = document.getElementById('popupPeriod');
-  var popupSubject = document.getElementById('popupSubject');
-  var popupDate = document.getElementById('popupDate');
-  var popupDiscription = document.getElementById('popupDiscription');
-
-  popupName.innerHTML = plan[value].name;
-  popupPeriod.innerHTML = plan[value].period;
-  popupSubject.innerHTML = plan[value].subject;
-  popupDate.innerHTML = plan[value].date;
-  popupDiscription.innerHTML = plan[value].discription;
-
-  var Screen = document.getElementById('screen');
-  Screen.style.display = "block";
-  var Popup = document.getElementById('popup');
-  Popup.style.display = "block";
-}
-
-var popupOff = function() {
-  var Screen = document.getElementById('screen');
-  Screen.style.display = "none";
-  var Popup = document.getElementById('popup');
-  Popup.style.display = "none";
-}
-
-var modify = function() {
-  popupOff();
-
-  var popupName = document.getElementById('popupName');
-  var popupSubject = document.getElementById('popupSubject');
-  var popupDate = document.getElementById('popupDate');
-  var popupDiscription = document.getElementById('popupDiscription');
-  var Value = "";
-
-  for (var i=0;i<plan.length;i++) {
-    if (popupName.innerHTML == plan[i].name || popupSubject.innerHTML == plan[i].subject ||
-        popupDate.innerHTML == plan[i].date || popupDiscription.innerHTML == plan[i].discription) {
-      var Value = i;
-      break;
-    }
-    else continue;
-  }
-
-  var date = document.getElementById("Date");
-  var period = document.getElementById("period");
-  var name = document.getElementById("name");
-  var subject = document.getElementById("subject");
-  var discription = document.getElementById("discription");
-
-  date.value = plan[Value].date;
-  period.value = plan[Value].period;
-  name.value = plan[Value].name;
-  subject.value = plan[Value].subject;
-  discription.value = plan[Value].discription;
-
-  deleteModule(Value);
 };
---
+++ public/firebaseFn.js
@@ -0,0 +1,167 @@
+var selectedKey;
+
+var config = {
+  apiKey: "AIzaSyCxT_364wsUH_-trxUIuvZa8bn5cxVsnZY",
+  authDomain: "manageassessment-77a67.firebaseapp.com",
+  databaseURL: "https://manageassessment-77a67.firebaseio.com/",
+  storageBucket: "gs://manageassessment-77a67.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var dbGet = function(date) {
+  var dataRef = database.ref(date + '/');
+  $('.planModule').remove();
+  dataRef.on('child_added', fnChildAdded);
+  dataRef.on('child_changed', fnChildChanged);
+}
+
+var fnChildAdded = function(data) {
+  console.log(data);
+  var key = data.key;
+  var information = data.val();
+  var name = information.name;
+  var subject = information.subject;
+
+  var date = $('#monthYear').text();
+
+  var Module = "<div class='planModule' id='"+key+"'>" +
+               "<button onclick=\"dbDelete('" + key + "')\">삭제</button>" +
+               "<button onclick=\"popupOn('" + key + "')\">자세히</button>" +
+               "<div>" +
+               "<span style='overflow-x: hidden' id='Name'>" + name + "</span> <br />" +
+               "<span id='Subject'>" + subject + "</span>" +
+               "</div>" +
+               "</div>";
+
+  $('#monthPlan').append(Module);
+}
+
+var fnChildChanged = function(data) {
+  var information = data.val();
+  var name = information.name;
+  var subject = information.subject;
+
+  $('#Name').text(name);
+  $('#Subject').text(subject);
+}
+
+var popupOn = function(key) {
+  var dataRef = database.ref($('#monthYear').text() + '/' + key)
+    .once('value').then(function(snapShot) {
+      $('#popupName').text(snapShot.val().name);
+      $('#popupPeriod').text(snapShot.val().period);
+      $('#popupSubject').text(snapShot.val().subject);
+      $('#popupDate').text(snapShot.val().date);
+      $('#popupDiscription').text(snapShot.val().discription);
+    });
+
+  $('.button').attr("id", key);
+
+  $('#screen').css('display', 'block');
+  $('#popup').css('display', 'block');
+}
+
+var popupOff = function() {
+  $('#screen').css('display', 'none');
+  $('#popup').css('display', 'none');
+}
+
+var dbDelete = function(key) {
+  var date = $('#monthYear').text();
+  var dataRef = database.ref(date + "/" + key);
+
+  dataRef.remove();
+  $("#" + key).remove();
+}
+
+var dbPush = function(jsonF) {
+  firebase.database().ref(jsonF.identifying).push(jsonF);
+}
+
+var dbUpdate = function(jsonF) {
+  firebase.database().ref(jsonF.identifying + '/' + selectedKey).update(jsonF);
+}
+
+var modify = function(key) {
+  popupOff();
+
+  var dataRef = database.ref($('#monthYear').text() + '/' + key)
+    .once('value').then(function(snapShot) {
+      $('#Date').val(snapShot.val().date);
+      $('#period').val(snapShot.val().period);
+      $('#name').val(snapShot.val().name);
+      $('#subject').val(snapShot.val().subject);
+      $('#discription').val(snapShot.val().discription);
+    });
+
+  selectedKey = key;
+}
+
+var clearForm = function() {
+  $('#Date').val('');
+  $('#period').val('');
+  $('#name').val('');
+  $('#subject').val('');
+  $('#discription').val('');
+
+  selectedKey = null;
+};
+
+var submitForm = function() {
+  var date = document.getElementById("Date");
+  var period = document.getElementById("period");
+  var name = document.getElementById("name");
+  var subject = document.getElementById("subject");
+  var discription = document.getElementById("discription");
+  var monthYear = document.getElementById("monthYear");
+
+  if (date.value == "" || name.value == "" || subject.value == "" || discription.value == "") {
+    if (date.value == "") {
+      var dateScript = document.getElementById('dateScript');
+      dateScript.innerHTML = "입력되지 않았습니다.";
+      setTimeout(function() {dateScript.innerHTML = ""}, 2000);
+    }
+    if (name.value == "") {
+      var nameScript = document.getElementById('nameScript');
+      nameScript.innerHTML = "입력되지 않았습니다.";
+      setTimeout(function() {nameScript.innerHTML = ""}, 2000);
+    }
+    if (subject.value == "") {
+      var subjectScript = document.getElementById('subjectScript');
+      subjectScript.innerHTML = "선택되지 않았습니다.";
+      setTimeout(function() {subjectScript.innerHTML = ""}, 2000);
+    }
+    if (discription.value == "") {
+      var discriptionScript = document.getElementById('discriptionScript');
+      discriptionScript.innerHTML = "입력되지 않았습니다.";
+      setTimeout(function() {discriptionScript.innerHTML = ""}, 2000);
+    }
+  }
+  else {
+    if (selectedKey) {
+      dbUpdate({
+        date: date.value,
+        period: period.value,
+        name: name.value,
+        subject: subject.value,
+        discription: discription.value,
+        identifying: monthYear.innerHTML
+      });
+    }
+    else {
+      dbPush({
+        date: date.value,
+        period: period.value,
+        name: name.value,
+        subject: subject.value,
+        discription: discription.value,
+        identifying: monthYear.innerHTML
+      });
+    }
+  }
+
+  clearForm();
+};
